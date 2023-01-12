41045052388a314407ffacaaa8d74f46577e00fc
F.K <94318094+fk00750@users.noreply.github.com>
2023-01-13 03:08:50 +0530

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -22,19 +22,19 @@ async function startServer() {
 
 startServer();
 
- if (cluster.isPrimary) {
-   // Create a single worker
-   cluster.fork();
+if (cluster.isPrimary) {
+  // Create a single worker
+  cluster.fork();
 
-   // Listen for exit events from workers and fork a new worker to replace the one that died
-   cluster.on("exit", (worker, code, signal) => {
-     // Check if there are no workers
-     if (!cluster.workers || Object.keys(cluster.workers).length === 0) {
-       // There are no workers, fork a new worker
+  // Listen for exit events from workers and fork a new worker to replace the one that died
+  cluster.on("exit", (worker, code, signal) => {
+    // Check if there are no workers
+    if (!cluster.workers || Object.keys(cluster.workers).length === 0) {
+      // There are no workers, fork a new worker
       cluster.fork();
-     }
-   });
- } else if (cluster.isWorker) {
-   // This is a worker process, start the server
-   startServer();
- }
+    }
+  });
+} else if (cluster.isWorker) {
+  // This is a worker process, start the server
+  startServer();
+}
